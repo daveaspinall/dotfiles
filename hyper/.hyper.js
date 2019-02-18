ccaf2b4b3cc206268e4058a566c1573a41e275ba
Dave Aspinall <dave.aspinall@codecomputerlove.com>
2019-02-18 09:14:55 +0000

    chore: update aliases and hyper config

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -1,61 +1,38 @@
 module.exports = {
   config: {
     fontSize: 14.5,
-    fontFamily: 'Source Code Pro, Lucida Console, monospace',
-    fontWeight: 'normal',
+    fontFamily: "Source Code Pro, Lucida Console, monospace",
+    fontWeight: "normal",
 
-    cursorColor: 'rgba(248,28,229,0.8)',
-    cursorShape: 'BEAM',
+    cursorColor: "rgba(248,28,229,0.8)",
+    cursorShape: "BEAM",
 
-    foregroundColor: '#fff',
-    backgroundColor: '#000',
-    borderColor: '#333',
+    foregroundColor: "#fff",
+    backgroundColor: "#000",
+    borderColor: "#333",
 
-    css: '',
+    css: "",
 
-    padding: '20px',
+    padding: "20px",
 
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
-
-    shell: '',
-    shellArgs: ['--login'],
+    shell: "",
+    shellArgs: ["--login"],
 
     env: {},
 
-    bell: 'SOUND',
+    bell: "SOUND",
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     copyOnSelect: false,
 
     hypercwd: {
-      initialWorkingDirectory: '~/Sites',
+      initialWorkingDirectory: "~/Sites"
     }
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
-  plugins: [
-    "hyperterm-base16-tomorrow-night",
-    "hyper-blink",
-    "hypercwd"
-  ],
+  plugins: ["hyperterm-base-16-ocean", "hyper-blink", "hypercwd"],
 
   localPlugins: []
 };
